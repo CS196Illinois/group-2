e6242e4d6d09f77f5dc4ab4bb945804d98ea2979
J Lee <ljin0906@gmail.com>
2020-10-18 15:18:45 -0500

    fix else in templates

--- aggregator/urls.py
+++ aggregator/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('addcourse',views.add_course,name = 'addcourse')
+    path('addcourse',views.add_course,name = 'addcourse'),
     path('logout/',views.user_logout, name='logout'),
     path('about/', views.about, name='about'),
 
