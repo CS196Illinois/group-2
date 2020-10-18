f97554e656273d7440bf46b8a72b77036e3d9cb6
J Lee <ljin0906@gmail.com>
2020-10-18 13:01:31 -0500

    fix urls for logout. (trailing fslash removed.)

--- aggregator/urls.py
+++ aggregator/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/logout',views.user_logout, name='logout')
+    path('logout/',views.user_logout, name='logout')
 ]
